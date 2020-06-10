7af7d6f0b83bcb52b8e2da815ade70ae75e72fe7
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-10 12:05:48 +0900

    投稿時間文字追加

--- index.php
+++ index.php
@@ -32,14 +32,14 @@ function readData() {
 }
 
 function writeData() {
-    //data_default_timezone_set('Asia/Tokyo');
+    //date_default_timezone_set('Asia/Tokyo');
     //date("Y/m/d H:i:s") . "\n";
     $personal_name = $_POST['personal_name'];
     $contents = $_POST['contents'];
     $contents = nl2br($contents);
 
     $data = "<hr>\n";
-    $data = $data."<p>".date("Y/m/d H:i:s") ."</p>\n";
+    $data = $data."<p>投稿日時".date("Y/m/d H:i:s") ."</p>\n";
     $data = $data."<p>投稿者:".$personal_name."</p>\n";
     $data = $data."<p>内容:</p>\n";
     $data = $data."<p>".$contents."</p>\n";
