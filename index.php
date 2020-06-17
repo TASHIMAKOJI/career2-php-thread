891c93b00b27a4ea92ced98605a1271720ddb20d
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-17 11:52:16 +0900

    bootstap

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <html>
 <head><title>掲示板</title></head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+
 <body>
 
-<h1>掲示板App</h1>
-
+ 
+<h1 class="text-center">掲示板App</h1>
+<br><br><br><br>
 <h2>投稿フォーム</h2>
 
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
     <input type="text" name="personal_name" placeholder="名前" required><br><br>
     <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
 </textarea><br><br>
-    <input type="submit" name="btn" value="投稿する">
+    <input type="submit" name="btn" value="投稿する" class="btn btn-primary">
 </form>
 
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
     <input type="hidden" name="method" value="DELETE">
-    <button type="submit">投稿をすべて削除する</button>
+    <button type="submit" class="btn btn-danger">投稿をすべて削除する</button>
 </form>
 <h2>スレッド</h2>
 
@@ -103,6 +109,12 @@ readData();
 
 
 ?>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 </html>
